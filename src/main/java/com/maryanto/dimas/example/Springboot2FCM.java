df2e49727539fa2e5451d05da85cf29372669118
dimasm93 <software.dimas_m@icloud.com>
2019-11-27 15:44:17 +0700

    refactor configuration

--- src/main/java/com/maryanto/dimas/example/Springboot2FCM.java
+++ src/main/java/com/maryanto/dimas/example/Springboot2FCM.java
@@ -1,13 +1,7 @@
 package com.maryanto.dimas.example;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
-import org.springframework.cloud.gcp.pubsub.integration.AckMode;
-import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.MessageChannel;
 
 @SpringBootApplication
 public class Springboot2FCM {
